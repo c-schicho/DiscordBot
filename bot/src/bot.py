b403fed7214528789c4948b4730c40ee5d229f17
christopher schicho <christopher.schicho@gmail.com>
2021-08-15 09:18:20 +0200

    3-added initial commands

--- bot/src/bot.py
+++ bot/src/bot.py
@@ -7,9 +7,44 @@ token = os.getenv("DISCORD_TOKEN")
 
 client = commands.Bot(command_prefix='!')
 
+is_active = False
+
 
 @client.event
 async def on_ready():
     print("\033[92mLOG: bot is up and ready\033[0m")
+    
+    
+@client.command()
+async def on(ctx):
+    global is_active
+    if is_active:
+        await ctx.send("Keep calm! I'm already active")
+    else:
+        is_active = True
+        await ctx.send("I'm active now!")
+    
+
+@client.command()
+async def off(ctx):
+    global is_active
+    if is_active:
+        is_active = False
+        await ctx.send("I'm going to sleep now. See you!")
+    else:
+        await ctx.send("Don't disturb me while I'm sleeping!")
+    
+
+@client.command()
+async def set_time(ctx, arg):
+    # TODO in #12
+    pass
+
+
+@client.command()
+async def info(ctx):
+    # TODO in #9
+    pass
+
 
 client.run(token)
